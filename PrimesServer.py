3c4ef6bfdc20b944ec5a8e193c0131813a4025e4
daajdk <dwegmann00@gmail.com>
2014-11-12 20:39:04 -0500

    Primes Server

---
+++ PrimesServer.py
@@ -0,0 +1,16 @@
+import rpyc
+
+class PrimesServer(rpyc.Service):
+def on_connect(self):
+    pass
+def on_disconnect(self):
+    pass
+def exposed_isPrime(num):
+    for k in range(2, num):
+    if (num % k) == 0: return False
+    return True
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(PrimesServer, port = 12345)
+    t.start()
