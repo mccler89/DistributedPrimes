efe8bba69c9711e3fc7317f0c722be54bee29e62
Patrick M <mccler89@students.rowan.edu>
2014-11-13 06:57:44 -0500

    Some changes

--- PrimesClient.py
+++ PrimesClient.py
@@ -1,1 +1,8 @@
 __author__ = 'kirtap'
+
+import rpyc
+
+server = "PrimesServer"
+
+c = rpyc.connect(server, port = 12345)
+c.root.isPrime(5)
--- PrimesServer.py
+++ PrimesServer.py
@@ -1,16 +1,18 @@
+
+
 import rpyc
 
 class PrimesServer(rpyc.Service):
-def on_connect(self):
-    pass
-def on_disconnect(self):
-    pass
-def exposed_isPrime(num):
-    for k in range(2, num):
-    if (num % k) == 0: return False
-    return True
+    def on_connect(self):
+        pass
+    def on_disconnect(self):
+        pass
+    def exposed_isPrime(num):
+        for k in range(2, num):
+            if (num % k) == 0: return False
+            return True
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(PrimesServer, port = 12345)
+    t = ThreadedServer(PrimesServer, port=12345)
     t.start()
